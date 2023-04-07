a627af3445b7c49de34592e67cd20ae97fa10990
Irechukwu Confidence <irechukwuconfidence@mail.com>
2023-04-07 02:25:47 +0100

    update user controller

--- controller/usersController.js
+++ controller/usersController.js
@@ -1,8 +1,12 @@
 import User from "../model/userModel.js"
+import bcrypt from "bcrypt";
+import generateToken from "../utils/generateToken.js";
+import { obtainTokenFromHeader } from "../utils/obtainTokenFromHeader.js";
+
 
 //create user account
 export const createUserController = async(req,res)=>{
-    const {firstname, lastname, profilephoto, email, password} = req.body
+    const {firstname, lastname, username, profilephoto, email, password} = req.body
     try{
       //check if user already exist
       const foundUser = await User.findOne({email});
@@ -13,30 +17,71 @@ export const createUserController = async(req,res)=>{
             message:"User with that email already exit"
           })
         }else{
+          //hash password
+          const salt = await bcrypt.genSalt(10);
+          const hashPassword = await bcrypt.hash(password,salt);
           const user = await User.create({
             firstname,
             lastname,
             email,
-            password
+            password:hashPassword,
           })
-        }
+        
       
       res.json({
           status:"success",
           data:user
       })
+    }
     } catch (error) {
       res.json(error.message);
     }
-  };
+  }
+
+
+  //login user
+export const userLoginCtrl = async (req,res)=>{
+  const {email,password} = req.body;
+  try { 
+      //get email
+      const isUserFound = await User.findOne({email});
+      if(!isUserFound){
+          return res.json({
+              message:"Wrong login credential",
+          })
+      }
+
+      //get password
+      const isPasswordFound = await bcrypt.compare(password,isUserFound.password);
+      if(!isPasswordFound){
+          return res.json({
+              message:"Wrong login credential"
+          })
+
+      }
+      res.json({
+          status:"success",
+          data:{
+          firstname: isUserFound.firstname,
+          lastname: isUserFound.lastname,
+          email: isUserFound.email,
+          token:generateToken(isUserFound._id)
+          }
+      })
+
+  }catch(error){
+      res.json(error.message);
+  }
+}
 
 
   //display all user
 export const displayAllController = async(req,res)=>{
     try{
+      const users = await User.find({});
       res.json({
           status:"success",
-          data:"Display all users"
+          data:users
       })
     } catch (error) {
       res.json(error.message);
@@ -47,13 +92,25 @@ export const displayAllController = async(req,res)=>{
   //profile
 export const profileController = async(req,res)=>{
     const userid = req.params.id;
+    //console.log(req.headers);
     try{
+      const token = obtainTokenFromHeader(req)
+      //console.log(token);
+      //console.log(userAuth);
+      const foundUser = await User.findById(userAuth);
+      if(!foundUser){
+        return res.json({
+          status:"error",
+          message:"No user associated with that id"
+        })
+      }
+
       res.json({
           status:"success",
-          data:"Get specific users "+userid
+          data:foundUser
       })
     } catch (error) {
-      res.json(error.message);
+      res.json(error.message)
     }
   };
 
