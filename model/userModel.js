de186cb2fc54abb602de0bc6df769a61ca729e6a
Irechukwu Confidence <irechukwuconfidence@mail.com>
2023-04-30 07:36:42 +0100

    updated

--- model/userModel.js
+++ model/userModel.js
@@ -9,6 +9,10 @@ const userSchema = new mongoose.Schema({
             type:String,
             required:[true, "Last name is required"],
         },
+        // username:{
+        //     type:String,
+        //     required:true,
+        // },
         profilephoto:{
             type:String,
         },
@@ -20,6 +24,10 @@ const userSchema = new mongoose.Schema({
             type:String,
             required:[true, "password is required"],
         },
+        isBlocked:{
+            type:Boolean,
+            default:false,
+        },
         isAdmin:{
             type:Boolean,
             default:false,
@@ -29,17 +37,78 @@ const userSchema = new mongoose.Schema({
             enum:["Admin", "Editor", "Guest"],
             default:"Guest"
         },
+        views:[
+            {
+                type:mongoose.Schema.Types.ObjectId,
+                ref:"User",
+            },
+        ],
+        followers:[
+            {
+                type:mongoose.Schema.Types.ObjectId,
+                ref:"User",
+            },
+        ],
+        following:[
+            {
+                type:mongoose.Schema.Types.ObjectId,
+                ref:"User",
+            },
+        ],
+        blocked:[
+            {
+                type:mongoose.Schema.Types.ObjectId,
+                ref:"User",
+            },
+        ],
         posts:[
         {
             type:mongoose.Schema.Types.ObjectId,
             ref:"Post"
         }
     ],
+    award:{
+        type:String,
+        enum:["Bronze","Silver","Gold"],
+        default:"Bronze",
+    },
     },
     {
         timestamps:true,
         toJSON:{virtuals:true}
     }
 );
+
+//get fullname
+userSchema.virtual("fullname").get(function(){
+    return `${this.firstname} ${this.lastname}`
+})
+
+//count post show the user how post he/she has posted
+userSchema.virtual("postCounts").get(function(){
+    return this.posts.length;
+})
+
+//followers count
+userSchema.virtual("followercount").get(function(){
+    return this.followers.length;
+})
+
+//following count
+userSchema.virtual("followingcount").get(function(){
+    return this.following.length;
+})
+
+//no of blocked user
+userSchema.virtual("blockuserCount").get(function(){
+    return this.blocked.length;
+})
+
+//initial incase if the user dose not a profile photo
+userSchema.virtual("initials").get(function(){
+    return `${this.firstname[0]}${this.lastname[0]}`
+})
+
+
 const User = mongoose.model("User",userSchema);
 export default User;
