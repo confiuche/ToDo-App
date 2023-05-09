0bd14547f1b4c24e6a5be25bf4cf454e4983de4c
Irechukwu Confidence <irechukwuconfidence@mail.com>
2023-05-09 12:29:34 +0100

    created admin delete route

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,6 +1,9 @@
 import express from 'express'
-import { createPostTaskController, deleteTaskCtrl, displayAllTask, updateTaskCtrl } from '../controller/postTaskController.js'
+import { createPostTaskController, deleteTaskByAdmin, deleteTaskCtrl, displayAllTask, displayTaskByAdmin, updateTaskCtrl } from '../controller/postTaskController.js'
 import { isLogin } from '../middlewares/isLogin.js'
+import { isAdmin } from '../middlewares/isAdmin.js';
+
+
 
 const taskRoutes = express.Router();
 
@@ -8,9 +11,13 @@ const taskRoutes = express.Router();
 taskRoutes.post("/create-task", isLogin, createPostTaskController)
 //display all task
 taskRoutes.get("/display-all-task/:id", isLogin, displayAllTask)
+//display all task
+taskRoutes.get("/admin-display-task", isLogin, isAdmin, displayTaskByAdmin)
 //update task
 taskRoutes.put("/update-account/:id", isLogin, updateTaskCtrl)
 //delete task
 taskRoutes.delete("/delete-task/:id", isLogin, deleteTaskCtrl)
+//delete task by admin
+taskRoutes.delete("/admin-delete-task/:id", isLogin, isAdmin, deleteTaskByAdmin)
 
 export default taskRoutes
