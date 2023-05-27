c76d6c958217fa01100a5c631bb33c03b5103e84
Irechukwu Confidence <irechukwuconfidence@mail.com>
2023-05-27 21:43:07 +0100

    added dependency

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv"
 import userRoutes from "./routes/userRoutes.js";
 import { database } from "./config/dBconnect.js";
 import taskRoutes from "./routes/taskRoutes.js";
+import cors from "cors"
+import globalErrorHandler from "./middlewares/globalErrorHandler.js"
 
 
 dotenv.config()
@@ -10,13 +12,33 @@ database()
 
 const app = express();
 
+app.use((req,res,next) => {
+    res.setHeader("Access-Contol-Allow-Origin", "*");
+    res.setHeader("Access-Contol-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
+    res.setHeader(
+        "Access-Content-Allow-Methods",
+        "GET,POST,PUT,DELETE,PATCH,OPTIONS"
+    );
+    next();
+})
+app.use(
+    cors({
+        credientials:true,
+        origin:true,
+        allowedHeaders: "*"
+    })
+)
+
 app.use(express.json());
+app.options("*", cors())
 
 const PORT = process.env.PORT || 8080;
 
 
 app.use("/api/v1/users",userRoutes);
 app.use("/api/v1/task",taskRoutes);
+
+app.use(globalErrorHandler)
 
 
 app.listen(PORT, console.log(`App started at ${PORT}`))
