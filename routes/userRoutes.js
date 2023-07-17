e0fa5437a0c5d73b4fe96f5f639b9a0f7bfcdc76
Irechukwu Confidence <irechukwuconfidence@mail.com>
2023-07-17 13:38:40 +0100

    updated

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,22 @@
 import express from 'express'
-import { createUserController, deleteUserController, displayAllController, forgetPasswordCtr, profileController, profilePhotoUploadCtr, resetPasswordCtr, updateUserController, userLoginCtrl } from '../controller/usersController.js';
+import { 
+    createUserController,
+    deleteUserController,
+    displayAllController,
+    forgetPasswordCtr, 
+    passwordSettingCtr, 
+    profileController, 
+    profilePhotoUploadCtr, 
+    resetPasswordCtr, 
+    updateUserController, 
+    userLoginCtrl 
+} from '../controller/usersController.js';
 import { isLogin } from '../middlewares/isLogin.js'
 import { validateUser } from '../middlewares/userValidation.js';
 import { isAdmin } from '../middlewares/isAdmin.js';
 import multer from 'multer';
 import storage from '../config/cloudinary.js';
+import initializePayment from '../controller/payStack.js';
 
 
 const upload = multer({storage})
@@ -28,9 +40,12 @@ userRoutes.put("/update-account",isLogin, updateUserController);
 //delete users
 userRoutes.delete("/delete-account",isLogin, deleteUserController);
 //forget password
-userRoutes.post("/forget-password", forgetPasswordCtr)
+userRoutes.post("/forget-password", forgetPasswordCtr);
 //reset password
 userRoutes.post("/reset-password", resetPasswordCtr)
+//password setting
+userRoutes.put("/security",isLogin, passwordSettingCtr)
+userRoutes.post('/acceptpayment', isLogin, initializePayment.acceptPayment);
 
 
 export default userRoutes;
